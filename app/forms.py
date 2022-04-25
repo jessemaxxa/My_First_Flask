fd670752cce054f7749ba72a9df2bf90b0193a87
root <root@kali.maxxaman>
2022-04-25 14:44:51 -0700

    added follow and unfollow users buttons in the app

--- app/forms.py
+++ app/forms.py
@@ -6,6 +6,11 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
